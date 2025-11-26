0472aada127fa1eb8d70838df3ea2087441327af
Greg Sullivan <gsullivan@draper.com>
2025-11-26 12:48:36 -0500

    did exercises

--- week1/handout/src/APL/Eval.hs
+++ week1/handout/src/APL/Eval.hs
@@ -1,6 +1,73 @@
 module APL.Eval
-  (
+  ( eval,
+    Val (..),
+    Env, envEmpty, envExtend, envLookup
   )
 where
 
-import APL.AST ()
+import APL.AST (Exp(..))
+
+type Error = String
+
+type VName = String
+
+type Env = [(VName, Val)]
+envEmpty :: Env
+envEmpty = []
+
+envExtend :: VName -> Val -> Env -> Env
+envExtend var val env = (var, val) : env
+
+envLookup :: VName -> Env -> Maybe Val
+envLookup = lookup
+
+data Val
+  = ValInt Integer
+  | ValBool Bool
+  deriving (Eq, Show)
+
+evalIntBinOp :: Env -> (Integer -> Integer -> Either Error Integer) -> Exp -> Exp -> Either Error Val
+evalIntBinOp env op e1 e2 =
+  case (eval e1 env, eval e2 env) of
+    (Left err, _) -> Left err
+    (_, Left err) -> Left err
+    (Right (ValInt i1), Right (ValInt i2)) -> case (i1 `op` i2) of
+      (Left err) -> Left err
+      (Right v) -> Right (ValInt v)
+    _ -> Left "at least one operand to int binop did not evaluate to an integer"
+
+eval :: Exp -> Env -> Either Error Val
+eval (CstInt i) _ = Right $ ValInt i
+eval (CstBool b) _ = Right $ ValBool b
+eval (Add e1 e2) env = evalIntBinOp env (\ i1 i2 -> Right $ i1 + i2) e1 e2
+eval (Sub e1 e2) env = evalIntBinOp env (\ i1 i2 -> Right $ i1 - i2) e1 e2
+eval (Mul e1 e2) env = evalIntBinOp env (\ i1 i2 -> Right $ i1 * i2) e1 e2
+eval (Div e1 e2) env = evalIntBinOp env checkedDiv e1 e2
+        where checkedDiv i1 i2
+                | i2 == 0 = Left "division by 0"
+                | otherwise = Right $ i1 `div` i2
+eval (Pow e1 e2) env = evalIntBinOp env checkedPow e1 e2
+  where checkedPow i1 i2
+          | i2 < 0 = Left "cannot raise to negative power"
+          | otherwise = Right $ i1 ^ i2
+eval (Eql e1 e2) env = case (eval e1 env, eval e2 env) of
+  (Left err, _) -> Left err
+  (_, Left err) -> Left err
+  (Right (ValBool b1), Right (ValBool b2)) -> Right $ ValBool $ (b1 == b2)
+  (Right (ValInt i1), Right (ValInt i2)) -> Right $ ValBool $ (i1 == i2)
+  _ -> Left "operands to eql do not match types"
+
+eval (If e1 e2 e3) env = case (eval e1 env) of
+  (Right (ValBool b)) -> if b then (eval e2 env) else (eval e3 env)
+  (Left err) -> Left err
+  _ -> Left "if predicate did not evaluate to a boolean"
+
+eval (Var vname) env = case (envLookup vname env) of
+  Nothing -> Left $ "variable " ++ vname ++ " not found in environment"
+  Just val -> Right val
+  
+eval (Let vname val_e in_e) env = case (eval val_e env) of
+  (Left err) -> Left err
+  (Right var_val) -> case (eval in_e (envExtend vname var_val env)) of
+    (Left err) -> Left err
+    (Right val) -> Right val
--- week1/handout/src/APL/Eval_Tests.hs
+++ week1/handout/src/APL/Eval_Tests.hs
@@ -1,12 +1,65 @@
 module APL.Eval_Tests (tests) where
 
-import APL.AST ()
-import APL.Eval ()
+import APL.AST (Exp (..))
+import APL.Eval (Val (..), Env, eval, envEmpty, envExtend, envLookup)
 import Test.Tasty (TestTree, testGroup)
-import Test.Tasty.HUnit ()
+import Test.Tasty.HUnit (testCase, (@?=))
+
+testEvalCstInt :: TestTree
+testEvalCstInt = testCase "CstInt 3 => ValInt 3" $ (eval (CstInt 3) envEmpty) @?= (Right $ ValInt 3)
+testEvalAdd :: TestTree
+testEvalAdd = testCase "add 1, 2 = 3" $ (eval (Add (CstInt 1) (CstInt 2)) envEmpty) @?= (Right $ ValInt 3)
+testEvalSub :: TestTree
+testEvalSub = testCase "sub 1, 2 = -1" $ (eval (Sub (CstInt 1) (CstInt 2)) envEmpty) @?= (Right $ ValInt (-1))
+testEvalMul :: TestTree
+testEvalMul = testCase "mul 1, 2 = 2" $ (eval (Mul (CstInt 1) (CstInt 2)) envEmpty) @?= (Right $ ValInt 2)
+testEvalDiv :: TestTree
+testEvalDiv = testCase "div 1, 2 = 0" $ (eval (Div (CstInt 1) (CstInt 2)) envEmpty) @?= (Right $ ValInt 0)
+testEvalPow :: TestTree
+testEvalPow = testCase "pow 1, 2 = 1" $ (eval (Pow (CstInt 1) (CstInt 2)) envEmpty) @?= (Right $ ValInt 1)
+testEvalDivZero :: TestTree
+testEvalDivZero = testCase "div 1 0 => error" $ (eval (Div (CstInt 1) (CstInt 0)) envEmpty) @?= (Left "division by 0")
+testEvalPowNeg :: TestTree
+testEvalPowNeg = testCase "pow 1, -2 => error" $
+                 (eval (Pow (CstInt 1) (CstInt (-2))) envEmpty) @?= (Left $ "cannot raise to negative power")
+testEvalCstBool :: TestTree
+testEvalCstBool = testCase "CstBool True => ValBool True" $ (eval (CstBool True) envEmpty) @?= (Right $ ValBool True)
+testEvalEql :: TestTree
+testEvalEql = testCase "Eql (CstInt 1) (Sub (CstInt 2) (CstInt 1)) == (ValBool True)" $
+              (eval (Eql (CstInt 1) (Sub (CstInt 2) (CstInt 1))) envEmpty) @?= (Right $ ValBool True)
+testEvalEqlNeg1 :: TestTree
+testEvalEqlNeg1 = testCase "Eql (CstInt 1) (Sub (CstBool True) (CstInt 1)) == error" $
+              (eval (Eql (CstInt 1) (Sub (CstBool True) (CstInt 1))) envEmpty) @?= 
+              (Left "at least one operand to int binop did not evaluate to an integer")
+testEvalEqlNeg2 :: TestTree
+testEvalEqlNeg2 = testCase "Eql (CstInt 1) (CstBool True) == error" $
+              (eval (Eql (CstInt 1) (CstBool True)) envEmpty) @?= 
+              (Left "operands to eql do not match types")
+testLetVar :: TestTree
+testLetVar = testCase "Let x (Add 1 2) (Add x 3) == 6" $
+             (eval (Let "x" (Add (CstInt 1) (CstInt 2)) (Add (Var "x") (CstInt 3))) envEmpty) @?=
+             (Right $ ValInt 6)
+testLetVarErr :: TestTree
+testLetVarErr = testCase "Let x (Add 1 2) (Add y 3) => error" $
+             (eval (Let "x" (Add (CstInt 1) (CstInt 2)) (Add (Var "y") (CstInt 3))) envEmpty) @?=
+             (Left "variable y not found in environment")
 
 tests :: TestTree
 tests =
   testGroup
     "Evaluation"
-    []
+    [ testEvalCstInt,
+      testEvalAdd,
+      testEvalSub,
+      testEvalMul,
+      testEvalDiv,
+      testEvalPow,
+      testEvalDivZero,
+      testEvalPowNeg,
+      testEvalCstBool,
+      testEvalEql,
+      testEvalEqlNeg1,
+      testEvalEqlNeg2,
+      testLetVar,
+      testLetVarErr
+    ]
